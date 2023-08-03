5ba660b7049bdba33a1a225e54831a431e30a3e6
Maikl <umnov.mixail@mail.ru>
2023-08-03 13:01:31 +0400

    ex010_011array

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,7 +1,7 @@
-﻿int[] array = { 1, 12, 44, 76, 89, 33, 54, 789 };
+﻿int[] array = { 1, 12, 44, 76, 89, 44, 33, 54, 789 };
 
 int n = array.Length;
-int find = 44;
+int find =44;
 
 int index = 0;
 
@@ -10,8 +10,10 @@ while (index < n)
     if(array[index] == find)
     {
         Console.WriteLine(index);
+        //если одинаковые значения в массиве и нам нужно только первое
+        break;
     }
-    //index = index + 1
+    //index = index + 1;
     index++; 
 
 }
