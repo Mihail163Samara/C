efaa1a33c6c73c973279897578372a3c255ea714
Maikl <umnov.mixail@mail.ru>
2023-08-03 12:00:46 +0400

    example009-010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 44, 76, 89, 33, 54, 789 };
+
+int n = array.Length;
+int find = 44;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index + 1
+    index++; 
+
+}
